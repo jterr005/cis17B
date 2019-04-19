89b6ba4113c0f4358267d418d8a0cb196697243c
JDTellz <48266212+JDTellz@users.noreply.github.com>
2019-04-19 14:38:32 -0700

    Update Ghost.h

--- src/Ghost/Ghost.h
+++ src/Ghost/Ghost.h
@@ -1,1 +1,21 @@
+#ifndef GHOST_H
+#define GHOST_H
 
+class Ghost
+{
+private:
+  int startX;
+  int startY;
+  int newPos;
+  int dir;
+ public:
+  Ghost();
+  void randGhost();
+  void scatter();
+  void chase();
+  void fear();
+  void death();
+  
+}
+
+#endif
