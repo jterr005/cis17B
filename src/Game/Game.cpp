ce0b9003ee18ffc675ecddf902a9e641a5c08a9b
Eriprez <eperez209@student.rccd.edu>
2019-04-21 23:59:35 -0700

    Signed-off-by: Eriprez <eperez209@student.rccd.edu>

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -1,15 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Game.cpp
- * Author: Eric Perez
- * 
- * Created on April 21, 2019, 1:56 PM
- */
 
 #include "Game.h"
 
--- src/Pacman/Pacman.h
+++ src/Pacman/Pacman.h
@@ -1,15 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Pacman.h
- * Author: Eric Perez
- *
- * Created on April 19, 2019, 5:20 AM
- */
 
 #ifndef PACMAN_H
 #define PACMAN_H
