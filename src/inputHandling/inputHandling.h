72b2d0b9e4a68990c59baadb4de4b3e954d904d2
jasont41 <43120995+jasont41@users.noreply.github.com>
2019-04-21 21:49:50 -0700

    Add files via upload

---
+++ src/inputHandling/inputHandling.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   inputHandling.h
+ * Author: Jason
+ *
+ * Created on April 15, 2019, 1:24 PM
+ */
+
+#ifndef INPUTHANDLING_H
+#define INPUTHANDLING_H
+#include <cstdlib>
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
+class inputHandling {
+public:
+    int getch(void); 
+private:
+    
+};
+
+#endif /* INPUTHANDLING_H */
+
